b176707043443bc7fe3ccf8373e3c33621eadb3f
James <wilfongjt@gmail.com>
2018-09-19 12:08:34 -0400

    re-run jurisdiction

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180916000001) do
+ActiveRecord::Schema.define(version: 20170926173203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
