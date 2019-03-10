c4a2a705fa7888e9f763be70b32ab367f6cd713f
Ross-Hunter <ross-hunter@ross-hunter.com>
2019-03-09 20:12:54 -0500

    Finishes Rspec configuration

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -4,15 +4,11 @@ RSpec.feature "Sign Up", type: :feature, js: true do
   it 'can be signed up on' do
     visit '/'
     click_on 'Register / Sign in'
-    fill_in 'user_email', with: 'test@example.com'
 
-    choose "I haven't signed up yet"
-
-    fill_in 'user_first_name', with: 'test@example.com'
-    fill_in 'user_last_name', with: 'test@example.com'
-
-    fill_in 'user_password_confirmation', with: 'foo-foo'
-
+    fill_in 'user[email]', with: 'test@example.com'
+    fill_in 'user[first_name]', with: 'Testy'
+    fill_in 'user[last_name]', with: 'Test Person'
+    fill_in 'user[password_confirmation]', with: 'foo-foo-bar'
     click_on "Sign up"
 
     expect(page).to have_content('Edit profile')
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,7 +55,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  # config.use_transactional_fixtures = true
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -76,4 +76,49 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    if config.use_transactional_fixtures?
+      raise(<<-MSG)
+        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+        (or set it to false) to prevent uncommitted transactions being used in
+        JavaScript-dependent specs.
+
+        During testing, the app-under-test that the browser driver connects to
+        uses a different database connection to the database connection used by
+        the spec. The app's database connection would not be able to access
+        uncommitted transaction data setup over the spec's database connection.
+      MSG
+    end
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    # :rack_test driver's Rack app under test shares database connection
+    # with the specs, so continue to use transaction strategy for speed.
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+
+    unless driver_shares_db_connection_with_specs
+      # Driver is probably for an external browser with an app
+      # under test that does *not* share a database connection with the
+      # specs, so use truncation strategy.
+      DatabaseCleaner.strategy = :truncation
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
