29df4b8389d7eb0fdbde0545533049071510e83e
James <wilfongjt@gmail.com>
2019-03-18 15:05:31 -0400

    removed unneeded comments

--- test/controllers/addresses_controller_test.rb
+++ test/controllers/addresses_controller_test.rb
@@ -2,17 +2,15 @@ require 'test_helper'
 
 class AddressesControllerTest < ActionController::TestCase
   test 'should return latitude and longitude for a valid address' do
-    # geocode_url='https://maps.google.com/maps/api/geocode/json'
     geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json'
     stub_request(:get, geocode_url).
-      with(query: {address: 'City Hall, Grand Rapids, MI', key: ENV["GOOGLE_MAPS_JAVASCRIPT_API_KEY"], sensor: 'false'}).
+      with(query: {address: 'City Hall, Grand Rapids, MI', sensor: 'false'}).
       to_return(body: File.read(File.expand_path('../../fixtures/city_hall.json', __FILE__)))
     get :show, address: 'City Hall', city_state: 'Grand Rapids, MI', format: 'json'
     assert_not_nil assigns :address
   end
 
   test 'should return an error for an invalid address' do
-    # geocode_url='https://maps.google.com/maps/api/geocode/json'
     geocode_url = 'https://maps.googleapis.com/maps/api/geocode/json'
     stub_request(:get, geocode_url).
       with(query: {address: ', ', key: ENV["GOOGLE_MAPS_JAVASCRIPT_API_KEY"], sensor: 'false'}).
