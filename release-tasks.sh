19e77451e0f84f6eab11f5d836d49d50508a7c74
James <wilfongjt@gmail.com>
2019-03-22 14:01:58 -0400

    migration fix

--- release-tasks.sh
+++ release-tasks.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo 'Starting data process'
-# bundle update;
-# bundle update rake;
-bundle exec rake db:create;
-bundle exec rake db:schema:load;
 bundle exec rake db:migrate;
-bundle exec rake data:load_things;
 echo 'Ending data process'
